638a155941ffaf57f4b99bb98ff972ad1b9b44a8
aiswaryakv2595 <aiswaryakv2595@gmail.com>
2023-08-07 12:55:09 +0530

    error resolved

--- server/index.js
+++ server/index.js
@@ -15,11 +15,12 @@ app.use(cors({ credentials: true, origin: 'https://strategix.netlify.app' }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "./public")));
 app.use(express.static(path.join(__dirname, '../client/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
+app.use(express.static(path.join(__dirname, "./public")));
+
 
 
 mongoose
