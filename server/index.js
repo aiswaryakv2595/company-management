b835148c31625e051b1203be6fa44a652c0af923
aiswaryakv2595 <aiswaryakv2595@gmail.com>
2023-08-07 14:35:21 +0530

    error resolved

--- server/index.js
+++ server/index.js
@@ -17,7 +17,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "./public")));
-
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 
 mongoose
